db73ffb9273a2aa138a6b7e78c173eb294a4c9b7
sebastien06400 <69351095+sebastien06400@users.noreply.github.com>
2020-11-08 12:05:32 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,159 +4,90 @@ array_love = []
 array_hate = []
 
 for (let x = 1; x < 9; x++) {
-image = document.getElementById(`${x}`)
-array.push(image)
+    image = document.getElementById(`${x}`)
+    array.push(image)
 
-a_icon = document.getElementById(`${x}a`)
-array_icon.push(a_icon)
+    a_icon = document.getElementById(`${x}a`)
+    array_icon.push(a_icon)
 
-a_love = document.getElementById(`love${x}`)
-array_love.push(a_love)
+    a_love = document.getElementById(`love${x}`)
+    array_love.push(a_love)
 
-a_hate = document.getElementById(`hate${x}`)
-array_hate.push(a_hate)
+    a_hate = document.getElementById(`hate${x}`)
+    array_hate.push(a_hate)
 }
 
 
-// iconslove = document.getElementsByClassName("iconlove")
 
 for (let x = 0; x < 8; x++) {
 
-array[x].addEventListener("mouseover", () => {   
-    array[x].style.filter = "saturate(0%)";
-    array[x].style.opacity = "0.3"
-    array_love[x].style.visibility = "visible";
-    array_hate[x].style.visibility = "visible";
-
-    // array_icon[x].style.visibility = "visible";
-    // array[x].style.zIndex = "-7";
-
-})
-
-array_love[x].addEventListener("mouseenter", () => {   
-    array_love[x].style.visibility = "visible";
-    array[x].style.opacity = "0.3"
-    array[x].style.filter = "saturate(0%)";
-})
-
-array_love[x].addEventListener("mouseout", () => {   
-    array_love[x].style.visibility = "visible";
-    array[x].style.opacity = "0.3"
-    array[x].style.filter = "saturate(0%)";
-    array[x].style.filter = "saturate(140%)";
-    array_love[x].style.visibility = "hidden";
-    array_hate[x].style.visibility = "hidden";
-    array[x].style.opacity = "1"
-})
-
-array_hate[x].addEventListener("mouseenter", () => {   
-    array_hate[x].style.visibility = "visible";
-    array[x].style.opacity = "0.3"
-    array[x].style.filter = "saturate(0%)";
-})
-
-array_hate[x].addEventListener("mouseout", () => {   
-    array_hate[x].style.visibility = "visible";
-    array[x].style.opacity = "0.3"
-    array[x].style.filter = "saturate(0%)";
-    array[x].style.filter = "saturate(140%)";
-    array_love[x].style.visibility = "hidden";
-    array_hate[x].style.visibility = "hidden";
-    array[x].style.opacity = "1"
-})
-
-array[x].addEventListener("mouseout", () => {   
-
-    array[x].style.filter = "saturate(140%)";
-    array_love[x].style.visibility = "hidden";
-    array_hate[x].style.visibility = "hidden";
-    array[x].style.opacity = "1"
+    array[x].addEventListener("mouseover", () => {
+        array[x].style.filter = "saturate(0%)";
+        array[x].style.opacity = "0.3"
+        array_love[x].style.visibility = "visible";
+        array_hate[x].style.visibility = "visible";
 
 
-})
+    })
 
-array_love[x].addEventListener("click", () => {   
-    bot = array[x].src
-    tops = array[x-1].src
-    array[x-1].src = bot
-    array[x].src = tops
-})
+    array_love[x].addEventListener("mouseenter", () => {
+        array_love[x].style.visibility = "visible";
+        array_hate[x - 1].style.visibility = "visible";
+        array[x].style.opacity = "0.3"
+        array[x].style.filter = "saturate(0%)";
+    })
 
-array_hate[x].addEventListener("click", () => {   
-    bot = array[x+1].src
-    tops = array[x].src
-    array[x+1].src = tops
-    array[x].src = bot
-})
+    array_love[x].addEventListener("mouseout", () => {
+        array_love[x].style.visibility = "visible";
+        array[x].style.opacity = "0.3"
+        array[x].style.filter = "saturate(0%)";
+        array[x].style.filter = "saturate(100%)";
+        array_love[x].style.visibility = "hidden";
+        array_hate[x].style.visibility = "hidden";
+        array_hate[x - 1].style.visibility = "hidden";
+        array[x].style.opacity = "1"
+    })
+
+    array_hate[x].addEventListener("mouseenter", () => {
+        array_hate[x].style.visibility = "visible";
+        array_love[x + 1].style.visibility = "visible";
+        array[x].style.opacity = "0.3"
+        array[x].style.filter = "saturate(0%)";
+    })
+
+    array_hate[x].addEventListener("mouseout", () => {
+        array_hate[x].style.visibility = "visible";
+        array[x].style.opacity = "0.3"
+        array[x].style.filter = "saturate(0%)";
+        array[x].style.filter = "saturate(100%)";
+        array_love[x].style.visibility = "hidden";
+        array_hate[x].style.visibility = "hidden";
+        array_love[x + 1].style.visibility = "hidden";
+        array[x].style.opacity = "1"
+    })
+
+    array[x].addEventListener("mouseout", () => {
+
+        array[x].style.filter = "saturate(100%)";
+        array_love[x].style.visibility = "hidden";
+        array_hate[x].style.visibility = "hidden";
+        array[x].style.opacity = "1"
+
+
+    })
+
+    array_love[x].addEventListener("click", () => {
+        bot = array[x].src
+        tops = array[x - 1].src
+        array[x - 1].src = bot
+        array[x].src = tops
+    })
+
+    array_hate[x].addEventListener("click", () => {
+        bot = array[x + 1].src
+        tops = array[x].src
+        array[x + 1].src = tops
+        array[x].src = bot
+    })
 
 }
-
-
-
-
-
-// var obj = JSON.parse('list.json');
-// console.log(obj);
-
-
-
-// const csv = from 'csv-parser';
-// console.log("ok")
-
-
-// //import dependencies
-// const fs = require("fs");
-// const csv = require('csvtojson');
-// const { Parser } = require('json2csv');
-
-// (async () => {
-
-//     //LOAD
-//     const list = await csv().fromFile("liste.csv");
-
-//     console.log(list)
-//     console.log("ok")
-// }
-// )
-
-
-/////////////////////////////////////////////
-// const reader = new FileReader()
-
-// reader.readAsText("liste.csv")
-
-// reader.onload = function () {
-//     console.log(reader.result)
-//     console.log("tost")
-// }
-/////////////////////////////////////////////
-// function getAsText(fileToRead) {
-//     let reader = new FileReader();
-//     reader.readAsText(fileToRead);
-//     reader.onload = loadHandler;
-
-// }
-
-// function loadHandler(event) {
-//     let csv = event.target.result;
-//     processData(csv);
-// }
-
-// let attendeesArray = []
-
-// function processData(csv) {
-//     let allTextLines = csv.split(/\r\n|\n)
-//     for (let i=0; i<allTextLines.length; i++) {
-//         let row = allTextLines[i].split(';');
-//         let col =[]
-//         for (let j=0; j<row.length;j++) {
-//             col.push(row[j])
-//         }
-
-//         attendeesArray.push(col)
-//     }
-// }
-
-
-
-
--- script2.js
+++ script2.js
@@ -1,25 +1,13 @@
-// const fs = require('fs');
 
-var requestURL = 'list.json';
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
 
-count = 1
+count = 0
 
 first = document.getElementById("first")
 second = document.getElementById("second")
 
-// request.onload = function() {
-    // col = document.getElementById(`user${count+1}`)
-    // var ladder = request.response;
-    // top8(ladder["name"], ladder["classement"], col)
-//   }
 
-
-  function top8(name, array, element) {
-    element.innerHTML = `
+function top8(name, array, element) {
+  element.innerHTML = `
     <div class="carte">${name}</div></br>
     <div class="carte"><img src="${array[0]}.png" width="80px"></div>
     <div class="carte"><img src="${array[1]}.png" width="80px"></div>
@@ -29,89 +17,104 @@ second = document.getElementById("second")
     <div class="carte"><img src="${array[6]}.png" width="80px"></div>
     <div class="carte"><img src="${array[7]}.png" width="80px"></div>
     `
-    count+=1;
-  }
+  count += 1;
+}
 
-  
-
-//   docu = document.documentElement
-console.log("start")
-  saved = document.getElementById("save")
-
-  saved.addEventListener('click', () => {
-      console.log("WHAT")
-  }
-  )
-
-  tost = document.getElementById("user1")
-
-  saved.addEventListener("click", () => {  
-      console.log("CLIqué") 
-      first.style.display = "none"
-      second.style.display = "flex"
-
-    //   tost.innerHTML = "LOUP"
-
-// var json = '{"name":"johnny","classement":"[1,1,1]}';
-
-//Parse the JSON: convert it into an object
-var hash = request.response
 
 username = document.getElementById('name').value
 
-ordered = []
-
-for (let k=1; k<9;k++) {
-    order = document.getElementById(`${k}`).src.match(/^.*(?=.png)/)[0].slice(-1)
-ordered.push(order)
-// console.log(Regex.Match(one.src, "^.*?(?=-)"))
-}
-// ^.*(?=.png)
-console.log(ordered)
-console.log("SRC")
+saved = document.getElementById("save")
+displayed = document.getElementById("display")
 
 
-hash.user2 = {"name":username,"classement":ordered}
-//add whatever you want
+
+saved.addEventListener("click", () => {
+  username = document.getElementById('name').value
+
+  if (username.length < 1) {
+    alert("Tu dois mettre un prénom !")
+  } else if (username.length > 9) {
+    alert("Raccourcis le prénom stp.")
+  } else {
+    console.log("CLIqué")
+    first.style.display = "none"
+
+
+    setTimeout(toast, 100);
+
+    async function toast() {
+      console.log("inside")
+      const response = await axios.get('https://jsonblob.com/api/get/7d103d16-21dc-11eb-a00e-53ad7aeed96a')
+
+      setTimeout(toast2, 100)
+
+      function toast2() {
+        let hash = (response.data);
+        console.log(hash)
+        second.style.display = "flex"
+
+
+
+        ordered = []
+
+        for (let k = 1; k < 9; k++) {
+          order = document.getElementById(k).src.match(/^.*(?=.png)/)[0].slice(-1)
+          ordered.push(order)
+        }
+        console.log(ordered)
+        console.log("SRC")
+
+        hash[`user${count+1}`] = {
+          "name": username,
+          "classement": ordered
+        }
+        console.log(hash)
+
+        let n = 0
+        while (hash[`user${n+1}`]) {
+          n += 1;
+          col = document.getElementById(`user${n}`)
+          top8(hash[`user${n}`]["name"], hash[`user${n}`]["classement"], col)
+
+        }
+        axios.put('https://jsonblob.com/api/get/7d103d16-21dc-11eb-a00e-53ad7aeed96a', hash)
+
+
+      }
+    }
+
+  }
+})
+
+
+displayed.addEventListener("click", () => {
+
+    first.style.display = "none"
+
+    setTimeout(toast, 10);
+
+    async function toast() {
+      const response = await axios.get('https://jsonblob.com/api/get/7d103d16-21dc-11eb-a00e-53ad7aeed96a')
+
+      setTimeout(toast2, 10)
+
+      function toast2() {
+        console.log("enter")
+        let hash = (response.data);
+        second.style.display = "flex"
 console.log(hash)
+        let n = 1
+        while (hash[`user${n+1}`]) {
+          n += 1;
+          col = document.getElementById(`user${n}`)
+          top8(hash[`user${n}`]["name"], hash[`user${n}`]["classement"], col)
 
-    let n = 0
-    while (hash[`user${n+1}`]) {
-        console.log("LOOP")
-        n+=1;
-    col = document.getElementById(`user${n}`)
-    console.log(col)
-    // var ladder = request.response;
-    console.log("ok")
-    top8(hash[`user${n}`]["name"], hash[`user${n}`]["classement"], col)
+        }
 
 
+      }
+    }
 
-
-    // fs.readFile('list.json', 'utf8', function readFileCallback(err, data){
-    //     if (err){
-    //         console.log(err);
-    //     } else {
-    //     obj = JSON.parse(data); //now it an object
-    //     obj.table.push({id: 2, square:3}); //add some data
-    //     json = JSON.stringify(obj); //convert it back to json
-    //     fs.writeFile('list.json', json, 'utf8', callback); // write it back 
-    // }});
-    
-
-    
   }
-  }
-  )
 
-
-
-// request.onload = function() {
-
-// console.log("oklii")
-
-
-// }
-
-  
-  
+)
